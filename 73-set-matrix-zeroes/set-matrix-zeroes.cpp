1b5aebbe633103c4492614d4a5aa36106d6fa6cd
dipeshdarks <dipeshgamercode@gmail.com>
2026-08-24 17:36:11 +0530

    Time: 0 ms (100.00%) | Memory: 21 MB (42.92%) - LeetSync

---
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& a) {
+        int m=a.size(), n=a[0].size(), r=0, c=0;
+        for(int i=0;i<m;i++) for(int j=0;j<n;j++)
+            if(a[i][j]==0) r|=(i==0), c|=(j==0), a[i][0]=a[0][j]=0;
+        for(int i=1;i<m;i++) for(int j=1;j<n;j++)
+            if(a[i][0]==0||a[0][j]==0) a[i][j]=0;
+        if(r) for(int j=0;j<n;j++) a[0][j]=0;
+        if(c) for(int i=0;i<m;i++) a[i][0]=0;
+    }
+};
